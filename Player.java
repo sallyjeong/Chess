8bd8aeedd15b4a34ed211ee38b03c7184a401dd1
Robby43189 <62026708+Robby43189@users.noreply.github.com>
2022-01-12 09:24:37 -0500

    Update Player.java
    Added basic structure of players. Subclasses of play (HumanPlayer, ComputerPlayer) can be added in the future

--- Player.java
+++ Player.java
@@ -1,5 +1,18 @@
 package chessproject;
 
 public class Player {
-	
+    private boolean white;
+    private boolean humanPlayer;
+    
+    public Player(boolean w, boolean h){
+        this.white = w;
+        this.humanPlayer = h;
+    }
+    
+    public boolean isWhite() {
+        if (white == true) {
+            return true;
+        }
+        return false;
+    }
 }
