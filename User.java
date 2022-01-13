bed7f7a8bb43c7b1f445702ea1699f2821b53ce5
sallyjeong <83872078+sallyjeong@users.noreply.github.com>
2022-01-13 09:11:47 -0500

    Add files via upload

---
+++ User.java
@@ -0,0 +1,11 @@
+public class User{
+    String username;
+
+    User(String username){
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
