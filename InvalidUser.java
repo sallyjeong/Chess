c6e004fa1a208c2889d92ee674dbeee58e854ed5
rachelyliu <71676708+rachelyliu@users.noreply.github.com>
2022-01-14 09:25:47 -0500

    Add files via upload

---
+++ InvalidUser.java
@@ -0,0 +1,50 @@
+package chess;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class InvalidUser extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InvalidUser frame = new InvalidUser();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public InvalidUser() {
+		setTitle("Invalid username");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 292, 200);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(238, 238, 238));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel invalidUser = new JLabel("Invalid username. Try again.");
+		invalidUser.setBounds(51, 68, 177, 16);
+		contentPane.add(invalidUser);
+	}
+
+}
