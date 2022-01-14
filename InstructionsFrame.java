d5b7f573fdca8069ad731b369519c7d41db512bc
rachelyliu <71676708+rachelyliu@users.noreply.github.com>
2022-01-13 22:56:15 -0500

    Update InstructionsFrame.java

--- InstructionsFrame.java
+++ InstructionsFrame.java
@@ -32,7 +32,7 @@ public class InstructionsFrame extends JFrame {
 	 */
 	public InstructionsFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1313, 715);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
