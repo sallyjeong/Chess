0896d31145174ec09156383616d9e7e27adc4ab2
rachelyliu <71676708+rachelyliu@users.noreply.github.com>
2022-01-18 17:00:25 -0500

    Add files via upload

--- HomeFrame.java
+++ HomeFrame.java
@@ -7,6 +7,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JList;
+
 import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -23,6 +25,10 @@ import javax.swing.UIManager;
 import java.awt.SystemColor;
 import java.nio.charset.IllegalCharsetNameException;
 import javax.swing.JSplitPane;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+
+import java.awt.ScrollPane;
 
 public class HomeFrame extends JFrame {
 
@@ -33,6 +39,7 @@ public class HomeFrame extends JFrame {
             public void run() {
                 try {
                     HomeFrame frame = new HomeFrame();
+                    frame.setVisible(true);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -58,13 +65,13 @@ public class HomeFrame extends JFrame {
         JPanel mainPanel = new JPanel();
         mainPanel.setBackground(new Color(255, 255, 255));
         mainPanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-        mainPanel.setBounds(14, 10, 184, 669);
+        mainPanel.setBounds(14, 10, 395, 669);
         contentPane.add(mainPanel);
         mainPanel.setLayout(null);
 
         //chess label
         JLabel chessLabel = new JLabel("Chess");
-        chessLabel.setBounds(71, 10, 40, 16);
+        chessLabel.setBounds(170, 6, 40, 16);
         chessLabel.setFont(new Font("Apple Braille", Font.PLAIN, 13));
         mainPanel.add(chessLabel);
 
@@ -76,7 +83,7 @@ public class HomeFrame extends JFrame {
                 new InstructionsFrame();
             }
         });
-        instructionsButton.setBounds(27, 133, 134, 45);
+        instructionsButton.setBounds(26, 175, 343, 80);
         mainPanel.add(instructionsButton);
 
         //settings button
@@ -87,112 +94,53 @@ public class HomeFrame extends JFrame {
                 new SettingsFrame();
             }
         });
-        settingsButton.setBounds(26, 65, 134, 45);
+        settingsButton.setBounds(26, 65, 343, 80);
         mainPanel.add(settingsButton);
 
         //lobby panel
         JPanel lobbyPanel = new JPanel();
         lobbyPanel.setBackground(new Color(255, 255, 255));
         lobbyPanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-        lobbyPanel.setBounds(207, 9, 1095, 669);
+        lobbyPanel.setBounds(426, 9, 876, 669);
         contentPane.add(lobbyPanel);
         lobbyPanel.setLayout(null);
 
         //lobby label
         JLabel lobbyLabel = new JLabel("Lobbies");
-        lobbyLabel.setBounds(175, 10, 57, 16);
+        lobbyLabel.setBounds(24, 7, 57, 16);
         lobbyPanel.add(lobbyLabel);
 
         JSeparator separator = new JSeparator();
-        separator.setBounds(3, 30, 1086, 12);
+        separator.setBounds(24, 31, 839, 12);
         lobbyPanel.add(separator);
 
         //public label
         JLabel publicLabel = new JLabel("Public ");
-        publicLabel.setBounds(15, 43, 61, 16);
+        publicLabel.setBounds(22, 52, 61, 16);
         lobbyPanel.add(publicLabel);
 
         JSeparator separator1 = new JSeparator();
-        separator1.setBounds(3, 203, 1086, 12);
+        separator1.setBounds(22, 326, 839, 12);
         lobbyPanel.add(separator1);
 
-        JButton enterRoomButton = new JButton("put variable for open server");
-        enterRoomButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-                String roomCode = "ahh"; // set the variable String roomCode based on the open server
-                // create a Client with existing roomCode variable
-
-                // new EnterUsernameFrame(roomCode);
-            }
-        });
-        enterRoomButton.setHorizontalAlignment(SwingConstants.LEFT);
-        enterRoomButton.setBounds(7, 69, 437, 23);
-        lobbyPanel.add(enterRoomButton);
-
         JSeparator separator2 = new JSeparator();
-        separator2.setBounds(3, 276, 1086, 12);
+        separator2.setBounds(22, 479, 839, 12);
         lobbyPanel.add(separator2);
-
-        JButton enterRoomButton2 = new JButton("put variable for open server");
-        enterRoomButton2.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-                // new ();    new frame here
-            }
-        });
-
-        enterRoomButton2.setHorizontalAlignment(SwingConstants.LEFT);
-        enterRoomButton2.setBounds(7, 100, 437, 23);
-        lobbyPanel.add(enterRoomButton2);
-
-        JButton enterRoomButton3 = new JButton("put variable for open server");
-        enterRoomButton3.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-                // new ();
-            }
-        });
-        enterRoomButton3.setHorizontalAlignment(SwingConstants.LEFT);
-        enterRoomButton3.setBounds(7, 130, 437, 23);
-        lobbyPanel.add(enterRoomButton3);
-
-        JLabel statusLabel = new JLabel("STATUS");
-        statusLabel.setBounds(456, 43, 61, 16);
-        lobbyPanel.add(statusLabel);
-
-        JLabel status = new JLabel("var");
-        status.setForeground(Color.RED);
-        status.setHorizontalAlignment(SwingConstants.CENTER);
-        status.setBounds(456, 72, 61, 16);
-        lobbyPanel.add(status);
-
-        JLabel status1 = new JLabel("var");
-        status1.setHorizontalAlignment(SwingConstants.CENTER);
-        status1.setForeground(new Color(50, 205, 50));
-        status1.setBounds(456, 102, 61, 16);
-        lobbyPanel.add(status1);
-
-        JLabel status2 = new JLabel("var");
-        status2.setHorizontalAlignment(SwingConstants.CENTER);
-        status2.setForeground(new Color(50, 205, 50));
-        status2.setBounds(456, 132, 61, 16);
-        lobbyPanel.add(status2);
 
         JButton playButton = new JButton("Enter matchmaking");
         playButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 frame.dispose();
-                new FindingRoomFrame(); // could also maybe replace with JOptionPanes
+                new FindingRoomFrame();
             }
         });
         playButton.setBackground(SystemColor.windowBorder);
-        playButton.setBounds(13, 160, 1062, 40);
+        playButton.setBounds(21, 229, 825, 80);
         lobbyPanel.add(playButton);
 
         //private label
         JLabel privateLabel = new JLabel("Private");
-        privateLabel.setBounds(19, 220, 61, 16);
+        privateLabel.setBounds(23, 352, 61, 16);
         lobbyPanel.add(privateLabel);
 
         //enter room code button (for private room)
@@ -200,26 +148,26 @@ public class HomeFrame extends JFrame {
         enterCodeButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 frame.dispose();
-                new Client();
+                // create Client with NO roomCode
+                // create new PrivateRoomCodeFrame() in Client to get and store the variable;
             }
         });
-        enterCodeButton.setBounds(11, 241, 510, 29);
+        enterCodeButton.setBounds(22, 384, 407, 80);
         lobbyPanel.add(enterCodeButton);
 
         JButton createRoomLabel = new JButton("Create room");
         createRoomLabel.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 frame.dispose();
-                // new EnterUsernameFrame(); create client to get ONLY username input
-                new CreatePrivateRoomFrame(); // this frame never works for kat lmao
+                new CreatePrivateRoomFrame();
             }
         });
-        createRoomLabel.setBounds(565, 241, 510, 29);
+        createRoomLabel.setBounds(469, 384, 373, 80);
         lobbyPanel.add(createRoomLabel);
 
         // playing computer label
         JLabel computerLabel = new JLabel("Computer");
-        computerLabel.setBounds(19, 290, 96, 16);
+        computerLabel.setBounds(23, 505, 96, 16);
         lobbyPanel.add(computerLabel);
 
         //play computer button
@@ -230,71 +178,35 @@ public class HomeFrame extends JFrame {
                 new PlayComputerFrame();
             }
         });
-
         playComputerButton.setBackground(SystemColor.windowBorder);
-        playComputerButton.setBounds(15, 315, 1060, 24);
+        playComputerButton.setBounds(23, 539, 825, 80);
         lobbyPanel.add(playComputerButton);
-
-        JButton enterRoomButton4 = new JButton("put variable for open server");
-        enterRoomButton4.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-                // new ();
-            }
+        
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setVisible(true);
+        scrollPane.setBounds(22, 82, 510, 129);
+        lobbyPanel.add(scrollPane);       
+        scrollPane.setBackground(new Color(143, 188, 143));
+        
+        String categories[] = { "Household", "Office", "Extended Family",
+                "Company (US)", "Company (World)", "Team", "Will",
+                "Birthday Card List", "High School", "Country", "Continent",
+                "Planet" };
+        JList list = new JList(categories);
+        scrollPane.setViewportView(list);
+        
+        JLabel lblNewLabel = new JLabel("Spectate public lobbies");
+        scrollPane.setColumnHeaderView(lblNewLabel);
+        
+        JButton confirmbUTTON = new JButton("Spectate");
+        confirmbUTTON.setForeground(new Color(143, 188, 143));
+        confirmbUTTON.setBackground(new Color(143, 188, 143));
+        confirmbUTTON.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		frame.dispose();
+        	}
         });
-        enterRoomButton4.setHorizontalAlignment(SwingConstants.LEFT);
-        enterRoomButton4.setBounds(565, 69, 437, 23);
-        lobbyPanel.add(enterRoomButton4);
-
-        JButton enterRoomButton5 = new JButton("put variable for open server");
-        enterRoomButton5.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-                // new ();
-            }
-        });
-        enterRoomButton5.setHorizontalAlignment(SwingConstants.LEFT);
-        enterRoomButton5.setBounds(565, 100, 437, 23);
-        lobbyPanel.add(enterRoomButton5);
-
-        JButton enterRoomButton6 = new JButton("put variable for open server");
-        enterRoomButton6.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                frame.dispose();
-                // new ();
-            }
-        });
-        enterRoomButton6.setHorizontalAlignment(SwingConstants.LEFT);
-        enterRoomButton6.setBounds(565, 130, 437, 23);
-        lobbyPanel.add(enterRoomButton6);
-
-        JLabel status3 = new JLabel("STATUS");
-        status3.setBounds(1014, 43, 61, 16);
-        lobbyPanel.add(status3);
-
-        JLabel status4 = new JLabel("var");
-        status4.setHorizontalAlignment(SwingConstants.CENTER);
-        status4.setForeground(Color.RED);
-        status4.setBounds(1014, 72, 61, 16);
-        lobbyPanel.add(status4);
-
-        JLabel status5 = new JLabel("var");
-        status5.setHorizontalAlignment(SwingConstants.CENTER);
-        status5.setForeground(new Color(50, 205, 50));
-        status5.setBounds(1014, 102, 61, 16);
-        lobbyPanel.add(status5);
-
-        JLabel status6 = new JLabel("var");
-        status6.setHorizontalAlignment(SwingConstants.CENTER);
-        status6.setForeground(new Color(50, 205, 50));
-        status6.setBounds(1014, 132, 61, 16);
-        lobbyPanel.add(status6);
-
-        JSeparator separator3 = new JSeparator();
-        separator3.setOrientation(SwingConstants.VERTICAL);
-        separator3.setBounds(529, 69, 12, 84);
-        lobbyPanel.add(separator3);
-
-        setVisible(true);
+        confirmbUTTON.setBounds(560, 114, 278, 65);
+        lobbyPanel.add(confirmbUTTON);
     }
 }
