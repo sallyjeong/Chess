075042ca9112b350a74a273ce5d00f286fe2c449
katherine liu <80860203+kkatherineliu@users.noreply.github.com>
2022-01-18 16:55:38 -0500

    Update PrivateRoomCodeFrame.java

--- PrivateRoomCodeFrame.java
+++ PrivateRoomCodeFrame.java
@@ -1,72 +1,74 @@
-package chessproject;
+NOT NEEDED ANYMORE
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+// package chessproject;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
+// import java.awt.BorderLayout;
+// import java.awt.EventQueue;
+// import java.awt.event.ActionEvent;
+// import java.awt.event.ActionListener;
 
-public class PrivateRoomCodeFrame extends JFrame {
+// import javax.swing.JFrame;
+// import javax.swing.JPanel;
+// import javax.swing.border.EmptyBorder;
+// import javax.swing.JLabel;
+// import javax.swing.JTextField;
+// import javax.swing.JButton;
 
-    private JPanel contentPane;
-    private JTextField codeText;
-    private JButton doneButton;
-    private String roomEntered;
+// public class PrivateRoomCodeFrame extends JFrame {
 
-//    public static void main(String[] args) {
-//        EventQueue.invokeLater(new Runnable() {
-//            public void run() {
-//                try {
-//                    PrivateRoomCodeFrame frame = new PrivateRoomCodeFrame();
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-//    }
+//     private JPanel contentPane;
+//     private JTextField codeText;
+//     private JButton doneButton;
+//     private String roomEntered;
 
-    /**
-     * Create the frame.
-     */
-    public PrivateRoomCodeFrame() {
-        JFrame frame = this;
-        setTitle("Enter room code");
-        setVisible(true);
+// //    public static void main(String[] args) {
+// //        EventQueue.invokeLater(new Runnable() {
+// //            public void run() {
+// //                try {
+// //                    PrivateRoomCodeFrame frame = new PrivateRoomCodeFrame();
+// //                } catch (Exception e) {
+// //                    e.printStackTrace();
+// //                }
+// //            }
+// //        });
+// //    }
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 292, 200);
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentPane);
-        contentPane.setLayout(null);
+//     /**
+//      * Create the frame.
+//      */
+//     public PrivateRoomCodeFrame() {
+//         JFrame frame = this;
+//         setTitle("Enter room code");
+//         setVisible(true);
 
-        JLabel enterCodeLabel = new JLabel("Room code:");
-        enterCodeLabel.setBounds(36, 52, 79, 16);
-        contentPane.add(enterCodeLabel);
+//         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//         setBounds(100, 100, 292, 200);
+//         contentPane = new JPanel();
+//         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+//         setContentPane(contentPane);
+//         contentPane.setLayout(null);
 
-        codeText = new JTextField();
-        codeText.setBounds(117, 47, 140, 26);
-        contentPane.add(codeText);
-        codeText.setColumns(10);
+//         JLabel enterCodeLabel = new JLabel("Room code:");
+//         enterCodeLabel.setBounds(36, 52, 79, 16);
+//         contentPane.add(enterCodeLabel);
 
-        doneButton = new JButton("Done");
-        doneButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                roomEntered = codeText.getText();
-                frame.dispose();
-            }
-        });
-        doneButton.setBounds(36, 92, 221, 29);
-        contentPane.add(doneButton);
-    }
+//         codeText = new JTextField();
+//         codeText.setBounds(117, 47, 140, 26);
+//         contentPane.add(codeText);
+//         codeText.setColumns(10);
 
-    public String getRoomEntered() {
-        return roomEntered.toLowerCase();
-    }
-}
+//         doneButton = new JButton("Done");
+//         doneButton.addActionListener(new ActionListener() {
+//             public void actionPerformed(ActionEvent e) {
+//                 roomEntered = codeText.getText();
+//                 frame.dispose();
+//             }
+//         });
+//         doneButton.setBounds(36, 92, 221, 29);
+//         contentPane.add(doneButton);
+//     }
+
+//     public String getRoomEntered() {
+//         return roomEntered.toLowerCase();
+//     }
+// }
