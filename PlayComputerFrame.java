d286dbd9a6a4c501cd2f4d0a617403b194b85ef5
rachelyliu <71676708+rachelyliu@users.noreply.github.com>
2022-01-18 10:45:53 -0500

    Add files via upload

--- PlayComputerFrame.java
+++ PlayComputerFrame.java
@@ -2,12 +2,15 @@ package chessproject;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
+import javax.swing.JButton;
 
 public class PlayComputerFrame extends JFrame {
 
@@ -29,24 +32,32 @@ public class PlayComputerFrame extends JFrame {
         contentPane.setLayout(null);
 
         JLabel colourLabel = new JLabel("Colour:");
-        colourLabel.setBounds(63, 46, 55, 16);
+        colourLabel.setBounds(63, 34, 55, 16);
         contentPane.add(colourLabel);
 
         JLabel levelLabel = new JLabel("Difficulty:");
-        levelLabel.setBounds(47, 91, 71, 16);
+        levelLabel.setBounds(47, 79, 71, 16);
         contentPane.add(levelLabel);
 
         String[] colours = {"black", "white", "random"};
 		JComboBox colourChoices = new JComboBox<>(colours);//use this one for drop down list
         //JComboBox colourChoices = new JComboBox();
-        colourChoices.setBounds(130, 42, 125, 27);
+        colourChoices.setBounds(130, 30, 125, 27);
         contentPane.add(colourChoices);
 
         String[] levels = {"easy", "medium", "hard"};
 		JComboBox difficulty = new JComboBox<>(levels);//use this one for drop down list
         //JComboBox difficulty = new JComboBox();
-        difficulty.setBounds(130, 87, 125, 27);
+        difficulty.setBounds(130, 75, 125, 27);
         contentPane.add(difficulty);
+        
+        JButton doneButton = new JButton("Done");
+        doneButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+        doneButton.setBounds(57, 118, 208, 29);
+        contentPane.add(doneButton);
     }
-
 }
