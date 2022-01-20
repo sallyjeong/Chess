34b8a956c5665372ac5dbaa4f68c56aba95bec0a
rachelyliu <71676708+rachelyliu@users.noreply.github.com>
2022-01-20 09:09:21 -0500

    Add files via upload

--- GameFrame.java
+++ GameFrame.java
@@ -1,53 +1,54 @@
 package chessproject;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.TitledBorder;
-import javax.swing.JScrollBar;
-import javax.swing.JButton;
 import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
-public class GameFrame extends JFrame {
+public class GameFrame {
 
-    private JPanel contentPane;
-//    public static void main(String[] args) {
-//        EventQueue.invokeLater(new Runnable() {
-//            public void run() {
-//                try {
-//                    GameFrame frame = new GameFrame();
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
-//    }
+	static Game game;
+	static Player curPlayer;
+	static Player p1, p2;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GameFrame game = new GameFrame();
 
-    /**
-     * Create the frame.
-     */
+	}
+	 
     public GameFrame() {
+    	JFrame frame = new JFrame();
+    	GamePanel board = new GamePanel();
+    	p1 = new Player(true, true); p2 = new Player(false, true);
+		curPlayer = p1;
+		game = new Game(p1, p2);
         // take in param to decide which way to display the board!!
         // take in param to see if its spectator or player (e.g. surrender button not available for spectators)
-        // take in param of room code to display in corner
-
-        JFrame frame = this;
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 1313, 715);
-        contentPane = new JPanel();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(100, 100, 1313, 715);
+        JPanel contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentPane);
+        frame.setContentPane(contentPane);
         contentPane.setLayout(null);
 
         JPanel boardPanel = new JPanel();
-        boardPanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
-        boardPanel.setBounds(674, 56, 576, 576); // 576/8 = 72
+        //boardPanel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+        boardPanel.add(board);
+        //board.setBounds(0,0, 576, 576);
+  
+        boardPanel.setBounds(674, 50, 600, 600); // 576/8 = 72
         contentPane.add(boardPanel);
+        //frame.add(board);
 
         JPanel movesPanel = new JPanel();
         movesPanel.setBorder(new TitledBorder(null, "Moves", TitledBorder.CENTER, TitledBorder.TOP, null, null));
@@ -84,16 +85,91 @@ public class GameFrame extends JFrame {
         surrenderButton.setBounds(246, 56, 172, 29);
         contentPane.add(surrenderButton);
 
-        JButton leaveButton = new JButton("Leave game");
-        leaveButton.addActionListener(new ActionListener() {
+        JButton flipButton = new JButton("Flip board");
+        flipButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                frame.dispose();
+            	
+//                frame.dispose();
                 // remove the user from game (if player, do the same thing as surrender)
                 // new ();    new frame here
             }
         });
-        leaveButton.setBounds(444, 56, 172, 29);
-        contentPane.add(leaveButton);
-        setVisible(true);
+        flipButton.setBounds(444, 56, 172, 29);
+        contentPane.add(flipButton);
+        frame.setVisible(true);
     }
+	
+	
+	public static class GamePanel extends JPanel implements MouseListener {
+		
+		private Spot source = null;
+		
+		public GamePanel() {
+			JPanel board = new JPanel();
+			//board.setBounds(0,0, 576, 576); // 576/8 = 72
+			setPreferredSize(new Dimension(576, 576));
+            addMouseListener(this);
+            setFocusable(true);
+            //requestFocusInWindow();
+        }
+		
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			game.getBoard().draw(g);
+			repaint();
+		}
+		
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if(e.getX()<8*game.getBoard().LENGTH && e.getY()<8*game.getBoard().LENGTH) {
+				Spot spot = game.getBoard().getBoard()[e.getY()/game.getBoard().LENGTH][e.getX()/game.getBoard().LENGTH];
+				if(source==null) {
+					if(spot.getPiece()!=null) {
+						spot.setClicked(true);
+						spot.getPiece().displayValidMoves(true);
+						source = spot;
+					}
+				}else {
+					if(spot.equals(source)) {
+						source.setClicked(false);
+						spot.getPiece().displayValidMoves(false);
+						source = null;
+					}else if(source.getPiece().getMoveList().contains(spot)) {
+						if(source.getPiece().isWhite()) {
+							game.playerMove(p1, source, spot);
+						}else {
+							game.playerMove(p2, source, spot);
+						}
+						source.setClicked(false);
+						if(source.getPiece()!=null) {
+							source.getPiece().displayValidMoves(false);
+						}
+						source = null;
+					}
+				}
+			}
+		}
+
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+		}
+
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+		}
+
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+		}
+
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+		}
+		
+	}
+	
 }
