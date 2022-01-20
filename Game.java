1bd8f224e746ff2fc55bd288f5df0485b391a123
peterg-xd <84744702+peterg-xd@users.noreply.github.com>
2022-01-20 17:59:45 -0500

    Add files via upload

--- Game.java
+++ Game.java
@@ -10,10 +10,10 @@ public class Game {
 	private Player turn;
 	private Board board;
 
-	public Game(boolean white, Player p1, Player p2) {
+	public Game(Player p1, Player p2) {
 		players[0] = p1;
 		players[1] = p2;
-		board = new Board(white);
+		board = new Board(p1.isWhite());
 		pastMoves = new ArrayList<Move>();
 		if(p1.isWhite()) {
 			this.turn = p1;
@@ -21,21 +21,21 @@ public class Game {
 			this.turn = p2;
 		}
 	}
-
+	
 	public boolean playerMove(Player player, Spot start, Spot end) {
 		Move move = new Move(player, start, end);
 		return makeMove(move);
 	}
 
 	private boolean makeMove(Move move) {
-		//	System.out.println(move.getStart().getRow()+" "+move.getStart().getColumn()+" "+move.getEnd().getRow()+" "+move.getEnd().getColumn());
+	//	System.out.println(move.getStart().getRow()+" "+move.getStart().getColumn()+" "+move.getEnd().getRow()+" "+move.getEnd().getColumn());
 		Piece sourcePiece = move.getStart().getPiece();
 		Player player = move.getPlayer();
-
+		
 		if(player!=turn) {
 			return false;
 		}
-
+		
 		Piece destPiece = move.getEnd().getPiece();
 		if(destPiece!=null) {
 			move.getEnd().removePiece();
@@ -46,9 +46,6 @@ public class Game {
 		sourcePiece.setMoved(true);
 		sourcePiece.setRow(move.getEnd().getRow()); sourcePiece.setCol(move.getEnd().getColumn());
 
-		Thread t= Thread.currentThread();
-
-
 		if (sourcePiece instanceof Pawn) {
 			boolean isWhite= sourcePiece.isWhite();
 			int lastRow=0;
@@ -56,49 +53,36 @@ public class Game {
 				lastRow+= 7;
 			}
 			if(sourcePiece.getRow() == lastRow){
-
 				PromotionFrame p= new PromotionFrame();
-
-
 				int choice= p.getChoice();
-
-				if(choice == 1){
-					move.getEnd().addPiece(new Queen(isWhite, false, 9, 'Q', sourcePiece.getRow(), sourcePiece.getCol()));
-				}
-
+					if(choice == 1){
+						move.getEnd().addPiece(new Queen(isWhite, false, 9, 'Q', sourcePiece.getRow(), sourcePiece.getCol()));
+					}
 			}
-
 		}
-
+		
 		if(move.isCastlingMove()) {
-			Piece movingRook;
-			Spot movingTo;
-			if(move.getEnd().getRow()==0) {
-				if(move.getEnd().getColumn()==2) {
-					movingRook = board.getBoard()[0][0].getPiece();
-					board.getBoard()[0][0].removePiece();
-					movingTo = board.getBoard()[0][3];
-				}else {
-					movingRook = board.getBoard()[0][7].getPiece();
-					board.getBoard()[0][7].removePiece();
-					movingTo = board.getBoard()[0][5];
-				}
+			Spot movingRook;
+			int row = move.getEnd().getPiece().getRow();
+			int col;
+			if(move.getEnd().getColumn()==2) {
+				col = 3;
+				movingRook = board.getBoard()[row][0];
+			}else if(move.getEnd().getColumn()==5) {
+				col = 4;
+				movingRook = board.getBoard()[row][7];
+			}else if(move.getEnd().getColumn()==6) {
+				col = 5;
+				movingRook = board.getBoard()[row][7];
 			}else {
-				if(move.getEnd().getColumn()==2) {
-					movingRook = board.getBoard()[7][0].getPiece();
-					board.getBoard()[7][0].removePiece();
-					movingTo = board.getBoard()[7][3];
-				}else {
-					movingRook = board.getBoard()[7][7].getPiece();
-					board.getBoard()[7][7].removePiece();
-					movingTo = board.getBoard()[7][5];
-				}
+				col = 2;
+				movingRook = board.getBoard()[row][0];
 			}
-			movingTo.addPiece(movingRook);
-			movingRook.setMoved(true);
-			movingRook.setRow(movingTo.getRow()); movingRook.setCol(movingTo.getColumn());
+			Piece rook = movingRook.removePiece();
+			board.getBoard()[row][col].addPiece(rook);
+			rook.setRow(row); rook.setCol(col);
+			System.out.println(board.getBoard()[row][col].getPiece().getRow()+ " "+board.getBoard()[row][col].getPiece().getCol());
 		}else if(move.isEnPassantMove()) {
-			System.out.println("yooo");
 			Spot above = board.getBoard()[move.getEnd().getRow()-1][move.getEnd().getColumn()];
 			if(above.getPiece() instanceof Pawn && ((Pawn)above.getPiece()).getEnPassant()) {
 				above.removePiece();
@@ -106,24 +90,24 @@ public class Game {
 				board.getBoard()[move.getEnd().getRow()+1][move.getEnd().getColumn()].removePiece();
 			}
 		}
-
-
+		
+		
 		pastMoves.add(move);
 		board.setEnPassant(!player.isWhite());
 		board.getPseudoLegal();
-
+		
 		if(this.turn==players[0]) {
 			this.turn = players[1];
 		}else {
 			this.turn = players[0];
 		}
-
+		
 		return true;
-
+		
 	}
-
-
-
+	
+	
+	
 	public Board getBoard() {
 		return this.board;
 	}
--- King.java
+++ King.java
@@ -54,35 +54,36 @@ public class King extends Piece {
 		}
 
 		canCastle(b);
-		
+
 		return super.getMoveList();
 
 	}
 
 	private void canCastle(Board b) {
 		Spot[][] board = b.getBoard();
-		if(isWhite()) {
-			if(!getMoved() && board[7][7].getPiece()!=null && !board[7][7].getPiece().getMoved()) {
-				if(board[7][5].getPiece()==null && !b.isThreatenedSpot(true, board[7][5]) && board[7][6].getPiece()==null && !b.isThreatenedSpot(true, board[7][6])) {
-					getMoveList().add(board[7][6]);
+		int row = getRow();
+		if(getCol()==4) {
+			if(!getMoved() && board[row][7].getPiece()!=null && !board[row][7].getPiece().getMoved()) {
+				if(board[row][5].getPiece()==null && !b.isThreatenedSpot(isWhite(), board[row][5]) && board[row][6].getPiece()==null && !b.isThreatenedSpot(isWhite(), board[row][6])) {
+					getMoveList().add(board[row][6]);
 				}
 			}
-			if(!getMoved() && board[7][0].getPiece()!=null && !board[7][0].getPiece().getMoved()) {
-				if(board[7][1].getPiece()==null && !b.isThreatenedSpot(true, board[7][1]) && board[7][2].getPiece()==null && !b.isThreatenedSpot(true, board[7][2])
-						&& board[7][3].getPiece()==null && !b.isThreatenedSpot(true, board[7][3])) {
-					getMoveList().add(board[7][2]);
+			if(!getMoved() && board[row][0].getPiece()!=null && !board[row][0].getPiece().getMoved()) {
+				if(board[row][1].getPiece()==null && !b.isThreatenedSpot(isWhite(), board[row][1]) && board[row][2].getPiece()==null && !b.isThreatenedSpot(isWhite(), board[row][2])
+						&& board[row][3].getPiece()==null && !b.isThreatenedSpot(isWhite(), board[row][3])) {
+					getMoveList().add(board[row][2]);
 				}
 			}
-		}else {
-			if(!getMoved() && board[0][7].getPiece()!=null && !board[0][7].getPiece().getMoved()) {
-				if(board[0][5].getPiece()==null && !b.isThreatenedSpot(false, board[0][5]) && board[0][6].getPiece()==null && !b.isThreatenedSpot(false, board[0][6])) {
-					getMoveList().add(board[0][6]);
+		}else if(getCol()==3) {
+			if(!getMoved() && board[row][0].getPiece()!=null && !board[row][0].getPiece().getMoved()) {
+				if(board[row][1].getPiece()==null && !b.isThreatenedSpot(isWhite(), board[row][1]) && board[row][2].getPiece()==null && !b.isThreatenedSpot(isWhite(), board[row][2])) {
+					getMoveList().add(board[row][1]);
 				}
 			}
-			if(!getMoved() && board[0][0].getPiece()!=null && !board[0][0].getPiece().getMoved()) {
-				if(board[0][1].getPiece()==null && !b.isThreatenedSpot(false, board[0][1]) && board[0][2].getPiece()==null && !b.isThreatenedSpot(false, board[0][2])
-						&& board[0][3].getPiece()==null && !b.isThreatenedSpot(false, board[0][3])) {
-					getMoveList().add(board[0][2]);
+			if(!getMoved() && board[row][7].getPiece()!=null && !board[row][7].getPiece().getMoved()) {
+				if(board[row][4].getPiece()==null && !b.isThreatenedSpot(isWhite(), board[row][4]) && board[row][5].getPiece()==null && !b.isThreatenedSpot(isWhite(), board[row][5])
+						&& board[row][6].getPiece()==null && !b.isThreatenedSpot(isWhite(), board[row][6])) {
+					getMoveList().add(board[row][5]);
 				}
 			}
 		}
