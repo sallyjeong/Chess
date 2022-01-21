f78b698ea7837c4fe2366f2632eae8be1af54089
katherine liu <80860203+kkatherineliu@users.noreply.github.com>
2022-01-21 09:26:26 -0500

    Add files via upload

--- Constants.java
+++ Constants.java
@@ -7,13 +7,16 @@ public class Constants {
     public static final String USERNAME_ERROR = "error. invalid username";
     public static final String JOIN_ROOM_ERROR = "error. invalid room"; //for private room
     public static final String QUICK_MATCH_WAIT = "waiting to be matched";
-    public static final char CHAT_DATA = '1';
-    public static final char MOVE_DATA = '2';
-    public static final char USERNAME_DATA = '3';
-    public static final char JOIN_PRIV_ROOM_DATA = '4';//for private room
-    public static final char CREATE_ROOM_DATA = '5';//for private room
-    public static final char QUICK_MATCH_DATA = '6';//for public room
-    public static final char JOIN_PUB_ROOM_DATA = '7';//for private room
-    public static final char COLOUR_DATA = '8';
+    public static final char CHAT_DATA = 'a';
+    public static final char MOVE_DATA = 'b';
+    public static final char USERNAME_DATA = 'c';
+    public static final char JOIN_PRIV_ROOM_DATA = 'd';//for private room
+    public static final char CREATE_ROOM_DATA = 'e';//for private room
+    public static final char QUICK_MATCH_DATA = 'f';//for public room
+    public static final char JOIN_PUB_ROOM_DATA = 'g';//for private room
+    public static final char COLOUR_DATA = 'h';
+    public static final char BOARD_DATA = 'i';
+    public static final char LEAVE_ROOM_DATA = 'j';
+    public static final char QUIT_DATA = 'k';
 
 }
