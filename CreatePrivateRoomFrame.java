744803cb07588251c4b1f5407000e686ab259116
sallyjeong <83872078+sallyjeong@users.noreply.github.com>
2022-01-22 16:32:44 -0500

    Add files via upload

--- CreatePrivateRoomFrame.java
+++ CreatePrivateRoomFrame.java
@@ -32,7 +32,7 @@ public class CreatePrivateRoomFrame extends JFrame {
         JFrame frame = this;
 
         setTitle("Creating private room");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         setBounds(100, 100, 292, 200);
         contentPane = new JPanel();
         contentPane.setBackground(new Color(238, 238, 238));
