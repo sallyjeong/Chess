02f985d03cadefdf6be759be60f9df3ba96ddfb1
katherine liu <80860203+kkatherineliu@users.noreply.github.com>
2022-01-25 18:44:00 -0500

    Update Drawable.java

--- Drawable.java
+++ Drawable.java
@@ -2,6 +2,11 @@ package chessproject;
 
 import java.awt.Graphics;
 
+/** [Drawable.java]
+ * An interface for all objects that can be drawn to the screen
+ * @author
+ * @version 1.0 Jan 25, 2021
+ */
 public interface Drawable {
 	
 	public void draw(Graphics g);
