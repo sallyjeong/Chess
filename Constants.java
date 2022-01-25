fd71399da5470e2078c46def48fee8471bdd05b8
katherine liu <80860203+kkatherineliu@users.noreply.github.com>
2022-01-25 17:50:45 -0500

    Add files via upload
    changed constants so changed these too (save as string instead bc ascii number was printed when move.toString for some reason

--- Constants.java
+++ Constants.java
@@ -1,15 +1,24 @@
 package chessproject;
 
-
+/** [Constants.java]
+ * Used to store Constant variables that are shared among various classes
+ * @author Katherine Liu, Sally Jeong
+ * @version 1.0 Jan 25, 2021
+ */
 public class Constants {
+    // networking
     public static final String HOST = "127.0.0.1"; //35.183.5.5
     public static final int PORT = 6000;
+
+    // responses
     public static final String USERNAME_ERROR = "error. invalid username";
     public static final String JOIN_ROOM_ERROR = "error. invalid room";
     public static final String QUICK_MATCH_WAIT = "waiting to be matched";
     public static final String QUICK_MATCH_JOINED = "joined game";
     public static final String REQUEST = "!request";
     public static final String DONE = "!done";
+
+    // data types
     public static final char CHAT_DATA = 'a';
     public static final char MOVE_DATA = 'b';
     public static final char USERNAME_DATA = 'c';
@@ -26,12 +35,14 @@ public class Constants {
     public static final char START_DATA = 'n';
     public static final char DRAW_DATA = 'o';
     public static final char GAME_OVER_DATA = 'p';
-    public static final char PAWN_INDICATOR = 'P';
-    public static final char CHECK = '+';
-    public static final char CHECKMATE = '#';
-    public static final char PROMOTE = '=';
+
+    // game and move symbols
+    public static final String PAWN_INDICATOR = "P";
+    public static final String CHECK = "+";
+    public static final String CHECKMATE = "#";
+    public static final String PROMOTE = "=";
+    public static final String MOVE = "-";
+    public static final String CAPTURE = "x";
     public static final String CASTLE_1 = "O-O";
     public static final String CASTLE_2 = "O-O-O";
-    public static final char MOVE = '-';
-    public static final String CAPTURE = "x";
 }
