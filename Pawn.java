3fe3e2bd14157f4295236a71efdabbd59a443b2e
swang142 <97454539+swang142@users.noreply.github.com>
2022-01-25 20:33:33 -0500

    Update Pawn.java

--- Pawn.java
+++ Pawn.java
@@ -8,7 +8,7 @@ import javax.imageio.ImageIO;
 
 /** [Pawn.java]
  * Represents each Pawn on the board
- * @author
+ * @author Stanley Wang
  * @version 1.0 Jan 25, 2022
  */
 public class Pawn extends Piece {
