d2e5494066d223735c73708c78335ef5628cf257
rachelyliu <71676708+rachelyliu@users.noreply.github.com>
2022-01-25 19:39:01 -0500

    Update Drawable.java

--- Drawable.java
+++ Drawable.java
@@ -5,7 +5,7 @@ import java.awt.Graphics;
 /** [Drawable.java]
  * An interface for all objects that can be drawn to the screen
  * @author
- * @version 1.0 Jan 25, 2021
+ * @version 1.0 Jan 25, 2022
  */
 public interface Drawable {
 	
