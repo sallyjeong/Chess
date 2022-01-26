2ac83b26865a1a2977da2dd10f4369ed09bf96e6
sallyjeong <83872078+sallyjeong@users.noreply.github.com>
2022-01-25 23:06:03 -0500

    Add files via upload

--- Client.java
+++ Client.java
@@ -10,7 +10,8 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.util.ArrayList;
 
-/** [Client.java]
+/**
+ * [Client.java]
  * Represents each person joining the chess program
  * Connects to the server and has the ability to play a chess game
  * @author Katherine Liu, Sally Jeong
@@ -170,7 +171,7 @@ public class Client extends Player {
         CreatePrivateRoomFrame roomFrame = new CreatePrivateRoomFrame(this);
         room = roomFrame.getCode();
         do {
-        	pause();
+            pause();
             colour = roomFrame.getColourChosen();;
         } while (roomFrame.isClosed()==false);
 
@@ -227,7 +228,7 @@ public class Client extends Player {
     public void pickSpectateColour() {
         EnterDataFrame colourChoice = new EnterDataFrame(Constants.COLOUR_DATA, this);
         do {
-        	pause();
+            pause();
             colour = colourChoice.getDataEntered().toLowerCase();
         } while (colourChoice.isClosed() == false);
 
@@ -265,10 +266,10 @@ public class Client extends Player {
             }
         }
     }
-
     /**
      * pause
      * Stops the main thread for a small portion of time
+     * to avoid
      */
     public void pause() {
         try {
@@ -278,7 +279,6 @@ public class Client extends Player {
         }
     }
 
-
     /*
     METHODS FOR PUBLIC ROOMS
      */
@@ -286,15 +286,20 @@ public class Client extends Player {
     /**
      * getRoomNames
      * Used to update the list of public rooms on the Home Frame
-     * @return an ArrayList of Strings, each holding the name of a room
+     * @return an Array of Strings, each holding the name of a room
      */
-    public ArrayList<String> getRoomNames(){
-        ArrayList <String> roomNames = new ArrayList<>();
+    public String[] getRoomNames(){
         sendData(Constants.ROOM_NAMES_DATA+"");
+        int size =0;
         try {
-            int size = Integer.parseInt(dataIn.readLine());
+            size = Integer.parseInt(dataIn.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String [] roomNames = new String[size];
+        try {
             for (int i = 0; i<size; i++){
-                roomNames.add(dataIn.readLine());
+                roomNames[i]=dataIn.readLine();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -388,7 +393,7 @@ public class Client extends Player {
                                 readMove(data);
                             } else if (type == Constants.UPDATE_LIST) {
                                 HomeFrame.roomNames = getRoomNames();
-                                HomeFrame.list.setListData(HomeFrame.returnRoomNames());
+                                HomeFrame.list.setListData(HomeFrame.roomNames);
                             } else if (type == Constants.BOARD_DATA) {
                                 if (isPlayer) {
                                     sendBoard(data);
@@ -440,7 +445,7 @@ public class Client extends Player {
             String endId;
 
             // check
-            if ((data.charAt(data.length()-1)+"").equals(Constants.CHECK)) {
+            if ((data.charAt(data.length()-1)+"") == Constants.CHECK) {
                 endId = data.substring(data.length() - 3, data.length() - 1);
                 if (isWhite()) {
                     board.setWhiteKingChecked(true);
@@ -456,7 +461,7 @@ public class Client extends Player {
                 }
 
                 // pawn promotion
-                if ((data.charAt(data.length()-2)+"").equals(Constants.PROMOTE)) {
+                if ((data.charAt(data.length()-2)+"") == Constants.PROMOTE) {
                     endId = data.substring(data.length() - 4, data.length() - 2);
                     char symbol = data.charAt(data.length() - 1);
                     promotePawn(startId, endId, symbol);
@@ -469,7 +474,7 @@ public class Client extends Player {
             }
 
             if (!pawnPromotion) {
-                if ((data.charAt(0)+"").equals(Constants.PAWN_INDICATOR)) { // checks for en passant
+                if ((data.charAt(0)+"") == Constants.PAWN_INDICATOR) { // checks for en passant
                     receiveMove(startId, endId, true);
                     data = " " + data.substring(1);
                 } else {
@@ -516,7 +521,7 @@ public class Client extends Player {
 
                     // special indexing for en passant
                     if (enPassant) {
-                        System.out.println(temp[i-1][j].removePiece());
+                        temp[i-1][j].removePiece();
                     }
                 }
             }
@@ -530,7 +535,7 @@ public class Client extends Player {
      * receiveMove
      * Modifies the Client's in-Game board based on opponent's move
      * Called when a castling move is received
-     * @param castle is the String holding the castling move (either CASTLE_1 or CASTLE_2 from Constants)
+     * @param castle is the String holding the castling move (either CASTLE_1 or CASTLE_1 from Constants)
      */
     public void receiveMove(String castle) {
         if (isWhite()) {
@@ -693,9 +698,13 @@ public class Client extends Player {
      *              example: 00Rw would mean a white Rook at index [0][0]
      */
     public void receiveBoard(String piece) {
+        if (piece.equals(Constants.DONE)) {
+
+        } else {
             // storing the piece data sent over
             int i = Character.getNumericValue(piece.charAt(0));
             int j = Character.getNumericValue(piece.charAt(1));
+
             char symbol = piece.charAt(2);
             char pieceColour = piece.charAt(3);
             boolean whitePiece = false;
@@ -705,25 +714,26 @@ public class Client extends Player {
             }
 
             // creating a new Piece based on data interpreted
-            if ((symbol+"").equals(Constants.PAWN_INDICATOR)) {
-                newPiece = new Pawn(whitePiece,  1, '\u0000', i, j, whitePiece);
+            if ((symbol + "").equals(Constants.PAWN_INDICATOR)) {
+                newPiece = new Pawn(whitePiece, 1, '\u0000', i, j, whitePiece);
             } else if (symbol == 'R') {
-                newPiece = new Rook(whitePiece,  5, symbol, i, j);
+                newPiece = new Rook(whitePiece, 5, symbol, i, j);
             } else if (symbol == 'N') {
-                newPiece = new Knight(whitePiece,  3, symbol, i, j);
+                newPiece = new Knight(whitePiece, 3, symbol, i, j);
             } else if (symbol == 'B') {
-                newPiece = new Bishop(whitePiece,  3, symbol, i, j);
+                newPiece = new Bishop(whitePiece, 3, symbol, i, j);
             } else if (symbol == 'Q') {
-                newPiece = new Queen(whitePiece,  9, symbol, i, j);
+                newPiece = new Queen(whitePiece, 9, symbol, i, j);
             } else if (symbol == 'K') {
-                newPiece = new King(whitePiece,  1000, symbol, i, j);
+                newPiece = new King(whitePiece, 1000, symbol, i, j);
                 if (whitePiece) {
-                    board.setWhiteKing((King)newPiece);
+                    board.setWhiteKing((King) newPiece);
                 } else {
-                    board.setBlackKing((King)newPiece);
+                    board.setBlackKing((King) newPiece);
                 }
             }
             board.getBoard()[i][j].addPiece(newPiece); // adding the new Piece to the correct Spot on the board
+        }
     }
 
     /**
@@ -867,8 +877,5 @@ public class Client extends Player {
     }
     public GameFrame getGameFrame() {
         return gameFrame;
-    }
-    public void setGameFrame(GameFrame gameFrame) {
-        this.gameFrame = gameFrame;
     }
 }
