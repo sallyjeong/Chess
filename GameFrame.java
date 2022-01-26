c3c1729d0664827cd38c2714971daded065d13bc
rachelyliu <71676708+rachelyliu@users.noreply.github.com>
2022-01-25 19:40:54 -0500

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -23,7 +23,7 @@ import java.awt.Color;
  * Main JFrama during gameplay
  * Includes chess board, display of past moves, chat box
  * @author Rachel Liu, Katherine Liu
- * @version 1.0 Jan 25, 2021
+ * @version 1.0 Jan 25, 2022
  */
 public class GameFrame extends JFrame {
 
